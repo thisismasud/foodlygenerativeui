6978c7b18bb9ef833e83222f371ee895ef4c2be3
Masud Parvez <thisismasud1@gmail.com>
2025-12-19 23:21:17 +0600

    fix versions and logout route

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,10 +1,10 @@
-import { Loader2Icon } from "lucide-react"
+import { Loader } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
-    <Loader2Icon
+    <Loader
       role="status"
       aria-label="Loading"
       className={cn("size-4 animate-spin", className)}
